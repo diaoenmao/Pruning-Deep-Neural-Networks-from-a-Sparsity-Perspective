4864515c763fb61c9d911f093789fb52a244911c
dem123456789 <diao_em@hotmail.com>
2022-02-17 00:29:16 -0500

    update sparsity index

--- src/modules/modules.py
+++ src/modules/modules.py
@@ -4,9 +4,13 @@ import time
 import sys
 import torch
 import models
+from collections import OrderedDict
 from config import cfg
 from utils import to_device, collate, make_optimizer, make_scheduler
 
 
-def make_sparsity_index():
-    return
+def make_sparsity_index(model, q):
+    sparsity_index = OrderedDict()
+    for k, v in model.state_dict().items():
+        sparsity_index[k] = torch.linalg.norm(v, 1, dim=-1) / torch.linalg.norm(v, q, dim=-1)
+    return sparsity_index
