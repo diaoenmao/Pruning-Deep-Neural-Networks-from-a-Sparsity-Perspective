b76036e1375131ac5acbfc4efac4c39fa22d6aea
dem123456789 <diao_em@hotmail.com>
2022-04-06 07:04:27 -0400

    update

--- src/make.sh
+++ src/make.sh
@@ -1,2 +1,11 @@
 python make.py --mode lt --model mlp --data MNIST --run train --round 8 --split_round 1
 python make.py --mode lt --model mlp --data MNIST --run test --round 8 --split_round 1
+
+python make.py --mode lt --model mlp --data FashionMNIST --run train --round 8 --split_round 1
+python make.py --mode lt --model mlp --data FashionMNIST --run test --round 8 --split_round 1
+
+python make.py --mode lt --model mlp --data CIFAR10 --run train --round 8 --split_round 1
+python make.py --mode lt --model mlp --data CIFAR10 --run test --round 8 --split_round 1
+
+python make.py --mode lt --model mlp --data CIFAR100 --run train --round 8 --split_round 1
+python make.py --mode lt --model mlp --data CIFAR100 --run test --round 8 --split_round 1
